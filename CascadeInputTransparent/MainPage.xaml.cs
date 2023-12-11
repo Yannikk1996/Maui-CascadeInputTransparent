942609f43e3fadb60bb607e28cf3cd9b1bb9133e
Yannikk1996 <33956727+Yannikk1996@users.noreply.github.com>
2023-12-11 10:35:36 +0100

    Add files via upload

---
+++ CascadeInputTransparent/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace CascadeInputTransparent
+{
+    public partial class MainPage : ContentPage
+    {
+        bool Transparent = false;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            Transparent = !Transparent;
+            if (Transparent)
+            {
+                CounterBtn.Text = $"Transparent";
+                ScrollContainer.InputTransparent = true ;
+            }
+            else
+            {
+                CounterBtn.Text = $"Inputs enabled";
+                ScrollContainer.InputTransparent = false;
+            }
+
+           
+        }
+    }
+
+}
